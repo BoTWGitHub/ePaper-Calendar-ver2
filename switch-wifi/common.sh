66824f44e42fdfd572d57c4ef3e32ee01762dbdd
Bo Lin <k2345777@gmail.com>
2025-04-15 20:18:36 +0800

    Reset whole project
    Add third party documents
    
    Signed-off-by: Bo Lin <k2345777@gmail.com>

--- switch-wifi/common.sh
+++
@@ -1,7 +0,0 @@
-
-#!/bin/bash
-
-HOTSPOT_NAME=rpi_hotspot
-HOTSPOT_SSID=rpi4_wifi
-HOTSPOT_PASSWD=ePaper1234
-DEFAULT_WIFI_NAME=preconfigured
--- switch-wifi/reset-to-default-wifi.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source common.sh
-
-sudo nmcli con down $HOTSPOT_NAME
-sudo nmcli con up $DEFAULT_WIFI_NAME
